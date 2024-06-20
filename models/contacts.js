a43dd129108607530c2fbd43ff736bd70de71799
Natalia Fedyk <124808493+Perslay@users.noreply.github.com>
2024-06-20 21:56:18 +0200

    Get contact by id
    200 if found
    404 if empty

--- models/contacts.js
+++ models/contacts.js
@@ -6,7 +6,14 @@ const listContacts = async () => {
   return contacts;
 };
 
-const getContactById = async (contactId) => {};
+const getContactById = async (contactId) => {
+  const contact = contacts.filter((contact) => contact.id === contactId);
+  if (contact.length !== 0) {
+    return contact;
+  } else {
+    return null;
+  }
+};
 
 const removeContact = async (contactId) => {};
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -21,7 +21,29 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const contactId = req.params.contactId;
+    const contact = await contactsFunctions.getContactById(contactId);
+    if (contact) {
+      res.json({
+        status: "success",
+        code: 200,
+        data: {
+          contact,
+        },
+        type: typeof contact,
+      });
+    } else {
+      res.status(404).json({
+        status: "failure",
+        code: 404,
+        message: "Not found",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 router.post("/", async (req, res, next) => {
